d2e8f985c33ad0cdb9da96e0a64a65ea54160f11
roshanmomand <1028086@hr.nl>
2024-12-02 15:12:09 +0100

    Navbar skeleton structure set

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -1,3 +1,7 @@
 <nav>
-    <a>Test for navigation</a>
+    <img src="../images/open-hiring-logo.png"  alt="Open Hiring logo">
+    <a href="">Alle vacatures</a>
+    <a href="">Over Open Hiring</a>
+    <a href="">Inspiratie</a>
+    <a href="">Contact</a>
 </nav>
