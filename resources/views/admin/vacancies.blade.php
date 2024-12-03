f1201c6fabb1ea6f36540474df1efe65d2b47ba5
LarsVerschoor <larsverschoor2@gmail.com>
2024-12-03 14:34:58 +0100

    403 if not admin

---
+++ resources/views/admin/vacancies.blade.php
@@ -0,0 +1,27 @@
+<x-layout title="OpenHiring | Vacatures">
+    <h2>Alle vacatures</h2>
+    <table>
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Functie</th>
+                <th>Uurloon</th>
+                <th>Contractduur</th>
+                <th>Aangemaakt</th>
+                <th>Bewerkt</th>
+            </tr>
+        </thead>
+        <tbody>
+        @foreach($allVacancies as $vacancy)
+            <tr>
+                <td>{{$vacancy->id}}</td>
+                <td>{{$vacancy->job_title}}</td>
+                <td>€{{$vacancy->paycheck}}</td>
+                <td>{{$vacancy->contract_term}}</td>
+                <td>{{$vacancy->created_at}}</td>
+                <td>{{date("m/d/y g:i A", strtotime($vacancy->updated_at))}}</td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+</x-layout>
