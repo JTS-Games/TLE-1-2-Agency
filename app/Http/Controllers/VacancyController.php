fa6d816631e5317c22bfdafa783df8f7d19c8ae7
Hidde-Scheringa <hescheringa@gmail.com>
2024-12-04 15:38:49 +0100

    Added a check for verify

--- app/Http/Controllers/VacancyController.php
+++ app/Http/Controllers/VacancyController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Company;
 use App\Models\Vacancy;
-use App\Models\Company;
 use Illuminate\Http\Request;
 
 
--- database/migrations/2024_12_02_131834_create_companies_table.php
+++ database/migrations/2024_12_02_131834_create_companies_table.php
@@ -15,6 +15,7 @@ return new class extends Migration {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('email')->unique();
             $table->binary('image')->nullable();
             $table->text('description');
             $table->string('location_hq');
